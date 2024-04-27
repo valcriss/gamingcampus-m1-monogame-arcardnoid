8961f65e9a5342a5bef8cebf0ff1b80d6e712d89
Daniel Silvestre <danielsilvestre.work@gmail.com>
2024-04-28 01:38:43 +0200

    update

---
+++ arcardnoid/Models/Content/Components/MainMenu/MainGameMenu.cs
@@ -0,0 +1,24 @@
+﻿using arcardnoid.Models.Framework.Components.UI;
+using arcardnoid.Models.Framework.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arcardnoid.Models.Content.Components.MainMenu
+{
+    public class MainGameMenu : Component
+    {
+        public MainGameMenu(int x,int y) : base("MainGameMenu", x, y)
+        {
+        }
+
+        public override void Load()
+        {
+            base.Load();
+            AddComponent(new Frame("frame","ui/banner", 0, 0, 576, 448));
+        }
+    }
+    
+}
--- arcardnoid/Models/Content/Scenes/MainMenu.cs
+++ arcardnoid/Models/Content/Scenes/MainMenu.cs
@@ -1,4 +1,6 @@
-﻿using arcardnoid.Models.Framework;
+﻿using arcardnoid.Models.Content.Components.MainMenu;
+using arcardnoid.Models.Framework;
+using arcardnoid.Models.Framework.Animations;
 using arcardnoid.Models.Framework.Components.Profiler;
 using arcardnoid.Models.Framework.Scenes;
 using Microsoft.Xna.Framework;
@@ -12,8 +14,9 @@ namespace arcardnoid.Models.Content.Scenes
         public MainMenu()
         {
             BackgroundColor = Color.FromNonPremultiplied(71, 171, 169, 255);
-            AddComponent(new Components.MainMenu.MainMenuBackground());
-            AddComponent(new Components.UI.AnimatedTitleBand("ui/bandeau", "fonts/band", "ArCardNoid", 0.09f, 960, 100, Color.FromNonPremultiplied(75, 30, 0, 255)));
+            AddComponent(new MainMenuBackground());
+            AddComponent(new Components.UI.AnimatedTitleBand("ui/bandeau", "fonts/band", "ArCardNoid : The Battle of the Bouncing Balls", 0.05f, 960, 100, Color.FromNonPremultiplied(75, 30, 0, 255)));
+            AddComponent(new MainGameMenu(672, 316)).AddAnimation(new MoveAnimation("MainGameMenuMove", 1f, new Vector2(672, 1081), new Vector2(672, 316), false, true));
             if (BaseGame.DebugMode) AddComponent(new ProfilerComponent());
         }
 
